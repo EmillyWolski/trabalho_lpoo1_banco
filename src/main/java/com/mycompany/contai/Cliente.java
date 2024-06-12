fd86d7cc291f6280b6858454a989225ead59f196
Emilly Wolski <emilly.wolski@gmail.com>
2024-06-12 20:41:28 -0300

    Inclusão de comentários nos códigos do projeto

--- src/main/java/com/mycompany/contai/Cliente.java
+++ src/main/java/com/mycompany/contai/Cliente.java
@@ -15,6 +15,7 @@ import java.util.Objects;
 
 
 public class Cliente {
+    // Atributos privados
     private String nome;
     private String sobreNome;
     private String rg;
@@ -22,6 +23,7 @@ public class Cliente {
     private String endereco;
     private Conta conta;
 
+    // Construtor
     public Cliente(String nome, String sobreNome, String rg, String cpf, String endereco) {
         this.nome = nome;
         this.sobreNome = sobreNome;
@@ -30,12 +32,15 @@ public class Cliente {
         this.endereco = endereco;
     }
 
+    // Getters que retornam (return) os valores dos atributos e
+    // Setters que permitem alteração (com .this definem ou atualizam o valor de um atributo) 
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+        // this para evitar ambiguidades quando os nomes dos parâmetros são iguais aos nomes dos atributos
     }
 
     public String getSobreNome() {
